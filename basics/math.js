38d742f5b99cc92508283648da330fb155acbc65
Vishwas Gour <162662951+Vishwas-gour@users.noreply.github.com>
2024-03-31 09:14:20 +0000

    remane file

---
+++ basics/math.js
@@ -0,0 +1,9 @@
+// console.log(Math.random()) // 0 to 1 in points
+// console.log(Math.random()*100) //0 to 100 in points
+// console.log(Math.random()*100+1) //1 to 100 in points
+// console.log(Math.floor((Math.random())*100+1)) //1 to 100 --floor conver (.) values to int value
+
+let min=10
+let max=30
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+                           

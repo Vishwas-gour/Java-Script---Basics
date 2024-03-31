ce466e0506e13169bfff0f5a974169a16c4b1324
Vishwas Gour <162662951+Vishwas-gour@users.noreply.github.com>
2024-03-31 10:06:02 +0000

    commit

--- basics/date.js
+++ basics/date.js
@@ -1,1 +1,8 @@
+let date=new Date()
+// console.log(date.toLocaleString());
+// console.log(date.getDate());
+let format=new Date("12-21-2024");
+console.log(format.toLocaleString())
+console.log(Math.floor(Date.now()/1000 ))  //in seconds
+console.log(Math.floor(Date.now()))  //in miliseconds
 
